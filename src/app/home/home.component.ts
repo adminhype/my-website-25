449a04ef07e2ee922b19692bb558d51734ab1673
Adem <ado.designs@outlook.de>
2025-08-18 16:59:08 +0200

    feat: start my skills section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent, AboutMeComponent],
+  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

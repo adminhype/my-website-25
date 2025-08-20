296aa5b4d168f7f289861b55a2d3e77b3bc1f565
Adem <ado.designs@outlook.de>
2025-08-20 11:27:14 +0200

    feat: base form structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

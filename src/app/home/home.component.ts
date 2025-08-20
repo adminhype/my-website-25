ac42acfa85c0a7a4478ab9208b4a97862a05ad1b
Adem <ado.designs@outlook.de>
2025-08-20 15:22:15 +0200

    feat: base footer structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,11 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

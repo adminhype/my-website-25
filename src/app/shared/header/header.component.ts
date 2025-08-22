79879c4776cbcfcb210b7c0cc81cbdf4b5a6b221
Adem <ado.designs@outlook.de>
2025-08-22 13:05:10 +0200

    feat: add header overlay for desktop

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,19 @@
-import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 
 }

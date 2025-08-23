b2336ba626f60cf84ffa134f15af5f4c3ee5b75d
Adem <ado.designs@outlook.de>
2025-08-23 21:00:36 +0200

    refactor: update my-skills

--- src/app/home/my-skills/my-skills.component.ts
+++ src/app/home/my-skills/my-skills.component.ts
@@ -1,11 +1,27 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Skills } from '../../interfaces/skills';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
 
+  basePath = './images/icons';
+
+  skills: Skills[] = [
+    { key: 'angular',  label: 'Angular',          icon: 'angular-icon.png' },
+    { key: 'ts',       label: 'TypeScript',       icon: 'ts-icon.png' },
+    { key: 'js',       label: 'JavaScript',       icon: 'js-icon.png' },
+    { key: 'html',     label: 'HTML',             icon: 'html-icon.png' },
+    { key: 'css',      label: 'CSS',              icon: 'css-icon.png' },
+    { key: 'git',      label: 'Git',              icon: 'git-icon.png' },
+    { key: 'firebase', label: 'Firebase',         icon: 'firebase-icon.png' },
+    { key: 'scrum',    label: 'Scrum',            icon: 'scrum-icon.png' },
+    { key: 'rest',     label: 'REST-API',         icon: 'rest-api-icon.png' },
+    { key: 'material', label: 'Material design',  icon: 'material-icon.png' },
+  ];
 }
---
+++ src/app/interfaces/skills.ts
@@ -0,0 +1,5 @@
+export interface Skills {
+    key: string;
+    label: string;
+    icon: string;
+}

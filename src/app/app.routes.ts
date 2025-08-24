568f47465e11c9f0b0a65ce4ceb2e973fdf60939
Adem <ado.designs@outlook.de>
2025-08-24 22:55:56 +0200

    feat: add legal notice and apply global header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component'
-import { HeroComponent } from './home/hero/hero.component';
+import { LegalNoticeComponent } from './home/legal-notice/legal-notice.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'header', component: HeaderComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../shared/header/header.component';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from "./contact/contact.component";
-import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
+  imports: [HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
 }

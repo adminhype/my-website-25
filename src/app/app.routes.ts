e09d62b8181ff4628a0c908d36703f1e080770f2
Adem <ado.designs@outlook.de>
2025-08-25 16:52:54 +0200

    style: responsiv design for legal notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './shared/header/header.component'
 import { LegalNoticeComponent } from './home/legal-notice/legal-notice.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'home', component: HomeComponent }
 ];

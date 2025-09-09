33d2eaceac92fb31d7fb73025afae432aaa5f5e5
Adem <ado.designs@outlook.de>
2025-09-09 18:18:39 +0200

    style: adjust content limit wide-screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from "./contact/contact.component";
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
